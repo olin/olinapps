e6724f6468e7c01464c74cb91e59184561e44709
Tim Cameron Ryan <id@timryan.org>
2012-12-04 17:21:44 -0500

    oliners

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-import os
-from flask import Flask, send_from_directory
-
+import os, bcrypt
+from flask import Flask, send_from_directory, jsonify
 from pymongo import Connection, ASCENDING, DESCENDING
 from bson.code import Code
 from bson.objectid import ObjectId
 
 #
-# MongoDB
+# Database
 #
 
-MONGO_URL = os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017/maumap')
+from urlparse import urlsplit
+MONGO_URL = os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017/olinapps-auth')
 parsed_mongo = urlsplit(MONGO_URL)
 db_name = parsed_mongo.path[1:]
 # Get db connection.
@@ -20,23 +20,33 @@ if '@' in MONGO_URL:
     user_pass = parsed_mongo.netloc.split('@')[0].split(':')
     db.authenticate(user_pass[0], user_pass[1])
 
+#
+# Passwords
+#
+
+def hash_password(password):
+  # Hash a password for the first time, with a randomly-generated salt
+  return bcrypt.hashpw(password, bcrypt.gensalt())
+
+def match_password(password, hashed):
+  return bcrypt.hashpw(password, hashed) == hashed
 
 #
-# Flask
+# Server
 #
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+  return jsonify(match=match_password('noise in our streets', hash_password('noise in our streets')))
+  return jsonify(words=[user['nothing'] for user in db.sessions.find()])
 
+# Serve index.html pages.
 @app.route('/<path:filename>')
 def pretty_static(filename):
   if filename.endswith('/'):
     filename += 'index.html'
-  else:
-    filename += '.html'
   return send_from_directory('static', filename)
 
 
@@ -47,4 +57,5 @@ def pretty_static(filename):
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
