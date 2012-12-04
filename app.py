19a8fdb4e6e8ebdf8d61ef7214c2c056c420c57d
Tim Cameron Ryan <id@timryan.org>
2012-12-04 16:26:57 -0500

    Adds pybcrypt

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
 import os
-from flask import Flask
+from flask import Flask, send_from_directory
+
+from pymongo import Connection, ASCENDING, DESCENDING
+from bson.code import Code
+from bson.objectid import ObjectId
+
+#
+# MongoDB
+#
+
+MONGO_URL = os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017/maumap')
+parsed_mongo = urlsplit(MONGO_URL)
+db_name = parsed_mongo.path[1:]
+# Get db connection.
+print('Connecting to %s [db %s]' % (MONGO_URL, db_name))
+db = Connection(MONGO_URL)[db_name]
+# Authenticate.
+if '@' in MONGO_URL:
+    user_pass = parsed_mongo.netloc.split('@')[0].split(':')
+    db.authenticate(user_pass[0], user_pass[1])
+
+
+#
+# Flask
+#
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/<path:filename>')
+def pretty_static(filename):
+  if filename.endswith('/'):
+    filename += 'index.html'
+  else:
+    filename += '.html'
+  return send_from_directory('static', filename)
+
+
+#
+# Launch
+#
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
