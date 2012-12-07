114eddd7d476f238f55c3cc17808586a4d4ccd66
Tim Cameron Ryan <id@timryan.org>
2012-12-07 03:44:27 -0500

    Yay

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os, bcrypt, uuid, time, requests
-from flask import Flask, send_from_directory, jsonify, session, request, redirect
+from urlparse import urlparse
+from flask import Flask, send_from_directory, jsonify, session, request, redirect, render_template
 from pymongo import Connection, ASCENDING, DESCENDING
 from bson.code import Code
 from bson.objectid import ObjectId
@@ -36,6 +37,10 @@ def match_password(password, hashed):
 # Server
 #
 
+"""
+On reset, we check if the username is a proxy name or not.
+"""
+
 app = Flask(__name__)
 app.secret_key = 'https://ia600808.us.archive.org/27/items/FoundryAndOlinPresentationByMichaelMoody/'
 
@@ -56,11 +61,29 @@ def reset_password(user):
 
   r = requests.post(MAILGUN_URL + "/messages", auth=("api", MAILGUN_KEY), data={
     "from": "no-reply@olinapps.mailgun.org",
-    "to": "tim@timryan.org",
+    "to": user['_id'] + '@' + user['domain'],
     "subject": "Olin Apps Password Reset",
     "text": "Please reset your password by following this link:\n\nhttp://auth.olinapps.com/reset?token=" + token
     })
-  return """An email has been sent to your account."""
+
+def ensure_user(email):
+  try:
+    user = db.users.find_one({
+      "_id": email_local_part(email)
+      })
+    if not user:
+      user = {
+        "_id": email_local_part(email),
+        "domain": email_domain_part(email),
+        "password": None,
+        "created": int(time.time()),
+        "resettoken": None,
+        "session": None
+        }
+      db.users.insert(user)
+    return user
+  except Exception:
+    return None
 
 def generate_session(user):
   # Session ID.
@@ -69,32 +92,61 @@ def generate_session(user):
     db.users.update({"_id": user['_id']}, user)
   session['sessionid'] = user['sessionid']
 
+def get_session_user():
+  if 'sessionid' not in session:
+    return None
+  return db.users.find_one({
+    "sessionid": session['sessionid']
+    })
+
 @app.route('/reset', methods=['GET', 'POST'])
 def route_reset():
-  user = db.users.find_one({"resettoken": request.args.get('token')})
+  user = db.users.find_one({
+    "resettoken": request.args.get('token')
+    })
   if not request.args.get('token') or not user:
-    return jsonify(message='Invalid token.')
+    if request.method == 'GET':
+      return render_template('reset.html')
+    else:
+      reset_password(ensure_user(request.form.get('email')))
+      return render_template('reset_sent.html')
 
-  if request.method == 'GET':
-    return send_from_directory('static', 'reset.html')
+  if request.args.get('token') and request.method == 'GET':
+    return render_template('reset_pass.html', user=user)
+
+  password = request.form.get('password', '')
+  if len(password) < 8:
+    return jsonify(message="Password must be >= 8 characters long.")
+
+  # Update user
+  user['password'] = hash_password(password)
+  user['resettoken'] = None
+  db.users.update({"_id": user['_id']}, user)
+  generate_session(user)
+
+  return redirect('/')
+
+@app.route('/')
+def route_index():
+  user = get_session_user()
+  return render_template('login.html',
+    external=request.args.get('external'),
+    user=user)
+
+@app.route('/external')
+def route_external():
+  user = get_session_user()
+  if user:
+    return render_template('external.html',
+      external=request.args.get('callback'),
+      sessionid=session['sessionid'])
   else:
-    password = request.form.get('password', '')
-    if len(password) < 8:
-      return jsonify(message="Password must be >= 8 characters long.")
+    return redirect('/?external=%s' % request.args.get('callback'))
 
-    # Update user
-    user['password'] = hash_password(password)
-    user['resettoken'] = None
-    db.users.update({"_id": user['_id']}, user)
-    generate_session(user)
-
-    # Redirect
-    return redirect('/')
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def route_login():
   # Normalize username.
-  username = request.form.get('username')
+  username = request.form.get('email')
   if not username:
     return jsonify(message="No such user or invalid password.")
 
@@ -103,55 +155,32 @@ def route_login():
     return jsonify(message='Invalid email address.')
 
   # Lookup user.
-  user = db.users.find_one({
-    "_id": email_local_part(username)
-    })
-
-  # Create user.
-  if not user:
-    user = {
-      "_id": email_local_part(username),
-      "password": None,
-      "created": int(time.time()),
-      "resettoken": None,
-      "session": None
-      }
-    db.users.insert(user)
+  user = ensure_user(username)
 
   # Reset or match password.
   if not user['password']:
-    return reset_password(user)
+    reset_password(user)
+    return render_template('reset_sent.html')
   if not match_password(request.form.get('password'), user['password']):
     return jsonify(message="No such user or invalid password.")
 
   generate_session(user)
 
-  if request.args.get('callback') and 'external' in request.args:
-    return redirect(request.args.get('callback') + '?sessionid=' + session['sessionid'])
+  #if request.args.get('callback') and 'external' in request.args:
+  #  return redirect(request.args.get('callback') + '?sessionid=' + session['sessionid'])
 
-  return jsonify(message="Logged in.", sessionid=user['sessionid'])
+  if request.args.get('external'):
+    return redirect('/external?%s' % request.args.get('external'))
+  return redirect('/')
 
-@app.route('/logout', methods=['GET', 'POST'])
+@app.route('/logout', methods=['POST'])
 def route_logout():
-  if 'sessionid' not in session:
-    return jsonify(message="You are not logged in.")
-  user = db.users.find_one({
-    "sessionid": session['sessionid']
-    })
+  user = get_session_user()
   if user:
     user.pop('sessionid', None)
     db.users.update({"_id": user['_id']}, user)
   session.pop('sessionid', None)
-  return jsonify(message="Session cleared.")
-
-# Serve index.html pages.
-def static_index(filename = ''):
-  if filename.endswith('/') or not filename:
-    filename += 'index.html'
-  return send_from_directory('static', filename)
-
-app.add_url_rule('/<path:filename>', 'static_index', static_index)
-app.add_url_rule('/', 'static_index', static_index)
+  return redirect('/')
 
 
 #
