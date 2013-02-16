8623746c208050ed4520d7035db78947b117e94c
Tim Cameron Ryan <id@timryan.org>
2013-02-16 18:30:10 -0500

    Session secrets

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.configure(function () {
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
+  app.use(express.cookieParser(process.env.SESSION_SECRET));
   app.use(express.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
