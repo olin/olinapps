09e8617e65bbb4c8e860b22ad70fc3f43b5de33b
Tim Cameron Ryan <id@timryan.org>
2013-02-17 15:29:58 -0500

    Olinapps 0.0.2

--- app.js
+++ app.js
@@ -7,23 +7,30 @@ var express = require('express')
   , http = require('http')
   , path = require('path')
   , olinapps = require('olinapps')
-  , mongojs = require('mongojs');
+  , mongojs = require('mongojs')
+  , MongoStore = require('connect-mongo')(express);
 
 var app = express(), db;
 
 app.configure(function () {
+  db = mongojs(process.env.MONGOLAB_URI || 'olinapps-quotes', ['quotes']);
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('secret', process.env.SESSION_SECRET || 'terrible, terrible secret')
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser(process.env.SESSION_SECRET));
-  app.use(express.session());
+  app.use(express.cookieParser(app.get('secret')));
+  app.use(express.session({
+    secret: app.get('secret'),
+    store: new MongoStore({
+      url: process.env.MONGOLAB_URI || 'mongodb://localhost/olinapps-quotes'
+    })
+  }));
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
-  db = mongojs(process.env.MONGOLAB_URI || 'olinapps-quotes', ['quotes']);
 });
 
 app.configure('development', function () {
@@ -42,6 +49,7 @@ app.configure('production', function () {
 app.post('/login', olinapps.login);
 app.all('/logout', olinapps.logout);
 app.all('/*', olinapps.middleware);
+app.all('/*', olinapps.loginRequired);
 
 /**
  * Routes
