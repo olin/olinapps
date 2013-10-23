095e39a6295cb374ff15e0f79a71e4a26c1a9b36
Tim Cameron Ryan <id@timryan.org>
2013-10-23 14:23:00 -0400

    Backup scripts

---
+++ tools/backup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+pushd .. 
+export MONGOURI="$(heroku config | grep MONGOLAB_URI | cut -c 16-)"
+popd
+export FOLDER=backups/$(date +'%Y-%m-%d')
+mkdir -p $FOLDER
+pushd $FOLDER
+mongoctl dump $MONGOURI
+popd
+
+echo
+echo BACKUP NOW LOCATED IN tools/$FOLDER
+echo "RESTORE WITH mongoctl restore $MONGOLAB_URI"
+echo "GOODBYE"
